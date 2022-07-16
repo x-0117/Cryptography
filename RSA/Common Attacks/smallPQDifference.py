0ff3540b2f9da9aae3094dde641a680767a0d661
x-0117 <72228744+x-0117@users.noreply.github.com>
2022-07-16 09:42:22 +0530

    Update smallPQDifference.py

--- RSA/Common Attacks/smallPQDifference.py
+++ RSA/Common Attacks/smallPQDifference.py
@@ -4,25 +4,26 @@
 # Theory : https://wiremask.eu/articles/fermats-prime-numbers-factorization/#:~:text=Fermat's%20factorization%20method%20uses%20that,prime%20numbers%20p%20and%20q.
 # Fermat's Factorisation
 # Useful when p and q differ by a small value.
+import gmpy2
 
-import numpy, gmpy2
-factors = []
+N = int(input("n : "))
+def fermat_factor(n):
+    assert n % 2 != 0
 
-n=int(input("n : "))
-if n%2==0:
-    factors.append(2)
-    factors.append(int(n/2))
+    a = gmpy2.isqrt(n)
+    b2 = gmpy2.square(a) - n
 
-else:
-    x=int(gmpy2.iroot(n, 2)[0]) + 1
-    while True:
-        y=int(gmpy2.iroot(numpy.power(x, 2)-n, 2)[0])
-        yf=int(y)
-        if (yf*yf==numpy.power(x, 2)-n):
-            factors.append(int(x+y))
-            factors.append(int(x-y))
-            break
-        else:
-            x=x+1
+    while not gmpy2.is_square(b2):
+        a += 1
+        b2 = gmpy2.square(a) - n
 
-print("Factors: " + str(factors))
+    p = a + gmpy2.isqrt(b2)
+    q = a - gmpy2.isqrt(b2)
+
+    return int(p), int(q)
+
+if __name__ == "__main__":
+    (p, q) = fermat_factor(N)
+
+    print("p = {}".format(p))
+    print("q = {}".format(q))
